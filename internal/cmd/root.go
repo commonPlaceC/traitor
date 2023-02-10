5faa10a2869e7897c36d7692b12e41995c14e9b9
Taileduser <aalex.03@mail.ru>
2023-02-11 00:56:35 +0300

    root edited

--- internal/cmd/root.go
+++ internal/cmd/root.go
@@ -30,7 +30,7 @@ func init() {
 
 var rootCmd = &cobra.Command{
 	Use:   "traitor",
-	Short: "Traitor is a privilege escalation framework for Linux",
+	Short: "Taractor is a privilege escalation framework for Linux",
 	Long: `An extensible privilege escalation framework for Linux
                 Complete documentation is available at https://github.com/liamg/traitor`,
 	Args: cobra.ExactArgs(0),
